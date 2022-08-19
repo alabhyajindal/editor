003e775c5619e0899e9889e5994cda49083557bc
Alabhya Jindal <alabhya10@gmail.com>
2022-08-20 04:17:00 +0530

    Init

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React, { SyntheticEvent, useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [content, setContent] = useState('');
+  const [selectedWord, setSelectedWord] = useState('');
+  const [isWordSelected, setIsWordSelected] = useState(false);
+
+  useEffect(() => {
+    if (isWordSelected && selectedWord !== '') console.log(selectedWord);
+  }, [isWordSelected, selectedWord]);
+
+  const getSelectedText = (
+    e: SyntheticEvent<HTMLTextAreaElement, Event>
+  ): void => {
+    const startPosition = e.target.selectionStart;
+    const endPosition = e.target.selectionEnd;
+    const selection = e.target.value.substring(startPosition, endPosition);
+    setSelectedWord(selection.trim());
+    setIsWordSelected(true);
+  };
+
+  const getSuggestions = (): void => {};
+
+  return (
+    <div className='App'>
+      <p>
+        Start writing. You'll get suggestions for words when you highlight one.
+      </p>
+      <textarea
+        onChange={(e) => {
+          setContent(e.target.value);
+          setIsWordSelected(false);
+        }}
+        onSelect={getSelectedText}
+      ></textarea>
+    </div>
+  );
+}
+
+export default App;
