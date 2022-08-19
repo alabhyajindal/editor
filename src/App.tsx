c0e8c406a9d94c8a6748fa1a6f68dc37f13197a0
Alabhya Jindal <alabhya10@gmail.com>
2022-08-20 04:57:02 +0530

    Added the API

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,37 @@ function App() {
   const [isWordSelected, setIsWordSelected] = useState(false);
 
   useEffect(() => {
-    if (isWordSelected && selectedWord !== '') console.log(selectedWord);
+    if (isWordSelected && selectedWord !== '') {
+      const getDefinition = async () => {
+        const definition = await getSuggestions(selectedWord);
+        definition !== undefined
+          ? console.log(definition)
+          : console.log('no definition found');
+      };
+      getDefinition();
+    }
   }, [isWordSelected, selectedWord]);
 
   const getSelectedText = (
     e: SyntheticEvent<HTMLTextAreaElement, Event>
   ): void => {
-    // const startPosition = e.target.selectionStart;
-    // const endPosition = e.target.selectionEnd;
-    // const selection = e.target.value.substring(startPosition, endPosition);
-    // setSelectedWord(selection.trim());
-    // setIsWordSelected(true);
+    const startPosition = e.target.selectionStart;
+    const endPosition = e.target.selectionEnd;
+    const selection = e.target.value.substring(startPosition, endPosition);
+    setSelectedWord(selection.trim());
+    setIsWordSelected(true);
   };
 
-  const getSuggestions = (): void => {};
+  const getSuggestions = async (word: string) => {
+    const endpoint = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${
+      import.meta.env.VITE_MERRIAM_WEBSTER_API_KEY
+    }`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const definition = data && data?.[0]?.shortdef?.[0];
+    return definition;
+  };
 
   return (
     <div className='App'>
